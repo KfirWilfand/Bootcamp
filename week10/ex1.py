e65539e612ea8894759b1fa250b96971d5219fc7
Kfir Wilfand <kfir333@gmail.com>
2019-07-14 14:24:40 +0300

    init week10

---
+++ week10/ex1.py
@@ -0,0 +1,7 @@
+from math import factorial as calcFactorial
+from math import exp as exponent 
+from math import ceil as ceiling
+
+assert(calcFactorial(5) == 120)
+print(exponent(3))
+print(ceiling(3.2))
---
+++ week10/ex2/main.py
@@ -0,0 +1,7 @@
+import moda 
+import modb 
+
+
+moda.oprint_name()
+modb.print_nickname()
+
